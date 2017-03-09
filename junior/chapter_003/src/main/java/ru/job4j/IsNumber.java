6df35288a34fa7bc2d19633b50c1070dcf5c0b1b
MicDubovik <michyy0101@gmail.com>
2017-03-09 15:52:57 +0300

    update

---
+++ junior/chapter_003/src/main/java/ru/job4j/IsNumber.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * Created by Katy on 09.03.2017.
+ */
+public class IsNumber {
+    public class Test003 {
+
+        public boolean isNumber(InputStream in) {
+
+            boolean result = false;
+            try {
+
+                System.out.println("Enter your number");
+                Scanner sc = new Scanner(System.in);
+                result = false;
+
+                if (Integer.parseInt(sc.nextLine()) % 2 == 0) {
+                    result = true;
+                }
+
+
+            } catch (Exception ex) {
+                ex.getMessage();
+            }
+            return result;
+        }
+    }
+}
