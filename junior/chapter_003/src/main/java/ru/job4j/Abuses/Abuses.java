8dc11f1abb85bfbdc1e2798335e87d9086b0221f
MicDubovik <michyy0101@gmail.com>
2017-03-09 19:56:55 +0300

    update

--- junior/chapter_003/src/main/java/ru/job4j/Abuses/Abuses.java
+++ junior/chapter_003/src/main/java/ru/job4j/Abuses/Abuses.java
@@ -1,19 +1,26 @@
 package ru.job4j.Abuses;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 /**
  * Created by Katy on 09.03.2017.
  */
 public class Abuses {
 
-    void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
+    void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String str=null;
+        for (String s : abuse) {
 
-        ByteArrayInputStream bis = new ByteArrayInputStream();
+            br.read(s.toCharArray());
+
+              str = s.replaceAll("abuses", "");
+        }
+
+        out.write(str.getBytes());
 
     }
+
 }
