8b0a8827b7704f637916ba11ecdda493a1b48391
MicDubovik <michyy0101@gmail.com>
2017-03-09 17:06:20 +0300

    update

---
+++ junior/chapter_003/src/main/java/ru/job4j/Abuses/Abuses.java
@@ -0,0 +1,19 @@
+package ru.job4j.Abuses;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by Katy on 09.03.2017.
+ */
+public class Abuses {
+
+    void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
+
+
+
+        ByteArrayInputStream bis = new ByteArrayInputStream();
+
+    }
+}
