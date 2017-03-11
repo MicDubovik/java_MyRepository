ff22e99fd66e7fa1a1de0cb0c738d64b2a65284f
MicDubovik <michyy0101@gmail.com>
2017-03-11 20:31:50 +0300

    update

---
+++ junior/chapter_003/src/main/java/ru/job4j/Abuses/Bot/Client.java
@@ -0,0 +1,54 @@
+package ru.job4j.Abuses.Bot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+   private Socket socket;
+   private int port ;
+
+
+    public Client(Socket socket) {
+        this.socket = socket;
+    }
+
+
+
+    public void startClient(){
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner console = new Scanner(System.in);
+
+            String ask = console.nextLine();
+            do {
+                if (ask.equals("By")){
+                    break;
+                }
+                out.println(ask);
+                String str ;
+                while (!(str = in.readLine()).isEmpty()) {
+                    System.out.println(str);
+                }
+            } while (true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 5000;
+        String ip = "127.0.0.1";
+        Socket socket = new Socket(InetAddress.getByName(ip), port);
+
+    }
+}
---
+++ junior/chapter_003/src/main/java/ru/job4j/Abuses/Bot/Server.java
@@ -0,0 +1,74 @@
+package ru.job4j.Abuses.Bot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Random;
+
+public class Server {
+    private Socket socket;
+    private final int port = 5000;
+    private final String[] asks = {"Hello", "By", "exit", "sure", "necessarily", "possible"};
+
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    public String selectAsk(String[] asks) {
+        Random random = new Random(5);
+        return asks[random.nextInt()];
+    }
+
+    public void startServer() {
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String ask;
+
+            do {
+                System.out.println("wait command ...");
+                ask = in.readLine();
+                System.out.println(ask);
+                if (ask.equals("by")){
+                    out.println("By");
+                    out.println();
+                }
+               else if ("hello".equals(ask)) {
+                    out.println("Hello, dear friend, I'm a oracle.");
+                    out.println();
+                }
+                 else  {
+                    out.println(selectAsk(asks));
+                    out.println();
+                }
+            } while ("by".equals(ask));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//    public static void main(String[] args) throws IOException {}
+
+
+
+//        Socket socket = new ServerSocket(port).accept();
+//        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//        do
+//
+//        {
+//            System.out.println("wait command ...");
+//            String ask = in.readLine();
+//            System.out.println(ask);
+//            if ("hello".equals(ask)) {
+//                out.println("Hello, dear friend, I'm a oracle.");
+//                out.println();
+//            }
+//        } while ("exit".
+//
+//                equals(ask));
+//    }
+
