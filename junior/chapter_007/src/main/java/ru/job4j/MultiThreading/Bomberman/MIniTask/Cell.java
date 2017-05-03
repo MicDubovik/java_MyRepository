a17f2ab5a43168c3767bd14a579a07b850b23c8b
MicDubovik <michyy0101@gmail.com>
2017-05-03 11:43:35 +0300

    update

--- junior/chapter_007/src/main/java/ru/job4j/MultiThreading/Bomberman/MIniTask/Cell.java
+++ junior/chapter_007/src/main/java/ru/job4j/MultiThreading/Bomberman/MIniTask/Cell.java
@@ -3,7 +3,7 @@ package ru.job4j.MultiThreading.Bomberman.MIniTask;
 import java.util.concurrent.locks.Lock;
 
 /**
- * Created by Katy on 02.05.2017.
+ * Synchronized locker !!!
  */
 public class Cell {
     /**
