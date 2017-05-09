efccf639913ba004a024e6ef9bb3a40e425a76e2
MicDubovik <michyy0101@gmail.com>
2017-05-09 20:11:21 +0300

    update

---
+++ junior/chaptre_008/src/main/java/ru/job4j/SQL_JDBC/AdditionalTask/TestQuery.sql
@@ -0,0 +1,2 @@
+﻿SELECT person.name,company.name  FROM  person  LEFT JOIN company ON person.company_id = company.id
+WHERE person.company_id<>5;
---
+++ junior/chaptre_008/src/main/java/ru/job4j/SQL_JDBC/AdditionalTask/TestQuery2.sql
@@ -0,0 +1,2 @@
+﻿SELECT company.name ,count(person.id) my_count FROM person,company WHERE person.company_id=company.id
+GROUP BY company.id,company.name;
