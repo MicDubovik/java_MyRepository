a6f9a3847ac2a2389f6a2e436867b6cb1fe300cc
MicDubovik <michyy0101@gmail.com>
2017-05-10 17:48:39 +0300

    update

---
+++ junior/chaptre_008/src/main/java/ru/job4j/SQL_JDBC/Cars/is_null_part_request.sql
@@ -0,0 +1,8 @@
+SELECT  *   FROM  body
+  LEFT OUTER JOIN  cars   ON body.id = cars.body WHERE cars.body ISNULL ;
+
+SELECT  *   FROM  transmission
+  LEFT OUTER JOIN  cars   ON transmission.id = cars.body WHERE cars.transmission ISNULL ;
+
+SELECT  *   FROM  engine
+  LEFT OUTER JOIN  cars   ON engine.id = cars.body WHERE cars.engine ISNULL ;
