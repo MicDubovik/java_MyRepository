2d0b590302978620211d7d106019dbf023b31326
MicDubovik <michyy0101@gmail.com>
2017-06-06 16:08:54 +0300

    update

--- junior/testProjectSpringBootData/src/main/java/ru/job4j/model/Role.java
+++ junior/testProjectSpringBootData/src/main/java/ru/job4j/model/Role.java
@@ -1,23 +1,24 @@
 package ru.job4j.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Table(name = "role")
 public class Role implements Serializable{
     @Id()
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
     private Long id;
 
     private String role;
 
-    @OneToMany(mappedBy = "role")
-    private Set<User> users = new HashSet<User>();
+    @OneToMany
+    @JoinColumn(name="user_id", updatable = false)
+    private List<User> users = new ArrayList<>();
 
     public Role() {
     }
@@ -26,6 +27,30 @@ public class Role implements Serializable{
         this.role = role;
     }
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 
     @Override
     public String toString() {
--- junior/testProjectSpringBootData/src/main/java/ru/job4j/model/User.java
+++ junior/testProjectSpringBootData/src/main/java/ru/job4j/model/User.java
@@ -16,6 +16,7 @@ public class User implements Serializable {
     private static final long serialVersionUID = -3009157732242241606L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private long id;
 
     private String name;
@@ -27,26 +28,93 @@ public class User implements Serializable {
     private String country;
     private String city;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Role role_id;
+    private Role role;
 
 
     protected User() {
     }
 
-    public User(String name, String login, String password, String email, String country, String city, Role role_id) {
+    public User(String name, String login, String password, String email, String country, String city, Role role) {
         this.name = name;
         this.login = login;
         this.password = password;
         this.email = email;
         this.country = country;
         this.city = city;
-        this.role_id = role_id;
+        this.role = role;
 
     }
 
 
+    @ManyToOne
+    @JoinColumn(name="role_id", updatable = false)
+    public Role getRole() {
+        return role;
+    }
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
     @Override
     public String toString() {
